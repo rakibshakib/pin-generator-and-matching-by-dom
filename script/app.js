a68b4d5d405000f4666a7c7b376a7c54eef02e0b
rakibshakib <rakibshakib74@gmail.com>
2021-08-18 16:12:39 +0600

    Pin genrerating finised and add logical part by DOM

--- script/app.js
+++ script/app.js
@@ -1,1 +1,47 @@
-console.log("script connected");
+function getPin() {
+    const pin = Math.round((Math.random() * 10000));
+    const pinStr = pin + '';
+    if (pinStr.length == 4) {
+        return pin;
+    } else {
+        return getPin();
+    }
+}
+
+function pinGenerating() {
+    const pin = getPin();
+    document.getElementById('pin-input').value = pin;
+}
+const pinButton = document.getElementById('pin-btn')
+pinButton.addEventListener('click', () => {
+    // pinButton.style.backgroundColor = 'yellow'
+    pinGenerating();
+})
+
+document.getElementById('cal-key-pad').addEventListener('click', (e) => {
+    const keyNumber = e.target.innerText;
+    const calcInput = document.getElementById('cal-input');
+    if (isNaN(keyNumber)) {
+        if (keyNumber == 'C') {
+            calcInput.value = '';
+        }
+    } else {
+        const perviousNumber = calcInput.value;
+        const newNumber = perviousNumber + keyNumber
+        calcInput.value = newNumber;
+    }
+})
+
+document.getElementById('submit-btn').addEventListener('click', ()=>{
+    const failedNotification = document.getElementById('failed');
+    const successNotification = document.getElementById('success');
+    const pinValue = document.getElementById('pin-input').value;
+    const inputValue = document.getElementById('cal-input').value;
+    if(pinValue == inputValue){
+        failedNotification.style.display = 'none';
+        successNotification.style.display = 'block';
+    }else{
+        failedNotification.style.display = 'block';
+        successNotification.style.display = 'none';
+    }
+})
